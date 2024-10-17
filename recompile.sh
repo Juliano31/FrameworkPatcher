2c3f168894df8a7d1c919ef1b34a4601d57792d5
Juliano31 <161245924+Juliano31@users.noreply.github.com>
2024-10-18 00:55:59 +0300

    Update recompile.sh

--- recompile.sh
+++ recompile.sh
@@ -1,3 +1,5 @@
 java -jar smali/smali/build/libs/smali.jar a -a 34 classes -o framework/classes.dex
 java -jar smali/smali/build/libs/smali.jar a -a 34 classes2 -o framework/classes2.dex
 java -jar smali/smali/build/libs/smali.jar a -a 34 classes3 -o framework/classes3.dex
+java -jar smali/smali/build/libs/smali.jar a -a 34 classes4 -o framework/classes4.dex
+java -jar smali/smali/build/libs/smali.jar a -a 34 classes5 -o framework/classes5.dex
